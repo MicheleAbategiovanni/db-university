f51ad368894bee48a0b7030384cd6cf142a2086b
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2023-01-12 16:27:56 +0100

    fix milestone 4

--- group_by.sql
+++ group_by.sql
@@ -11,6 +11,6 @@ SELECT COUNT(id), `office_address` FROM `teachers` GROUP BY `office_address`;
 3. Calcolare la media dei voti di ogni appello d'esame
 
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT id, COUNT(id) as totale_corsi
-FROM `degrees`
-GROUP BY `department_id`;
+SELECT COUNT(id) as totale_corsi, GROUP_CONCAT(id) as id_corsi
+FROM degrees
+GROUP BY department_id;
