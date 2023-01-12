e6818b35e024d5d5dd653218700da8dd5a32b982
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2023-01-12 16:49:51 +0100

    complete milestone 4

--- join.sql
+++ join.sql
@@ -5,22 +5,38 @@ INNER JOIN `students`
 ON `students`.`degree_id` = `degrees`.`department_id`
 WHERE `department_id` = "9";
 
-SELECT * FROM `degrees` INNER JOIN `students` ON `students`.`degree_id` = `degrees`.`department_id` WHERE `degrees`.`name` = "Corso di Laurea in Economia"; 
+SELECT  *
+FROM `degrees`
+INNER JOIN `students`
+ON `students`.`degree_id` = `degrees`.`department_id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia"; 
 
-2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze 
-SELECT * FROM `degrees` INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id` WHERE `degrees`.`department_id` = 7 AND `degrees`.`level` = "magistrale"; 
+2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
 
-3. Selezionare tutti i corsi IN cui insegna Fulvio Amato (id = 44) 
-SELECT * FROM `course_teacher` INNER JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id` WHERE `teacher_id` = "44"; 
+SELECT  *
+FROM `degrees`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`department_id` = 7
+AND `degrees`.`level` = "magistrale"; 
 
-4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, IN ordine alfabetico per cognome e nome 
+3. Selezionare tutti i corsi IN cui insegna Fulvio Amato (id = 44)
+
+SELECT  *
+FROM `course_teacher`
+INNER JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teacher_id` = "44"; 
+
+4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, IN ordine alfabetico per cognome e nome
+
+SELECT  *
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `degree_id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`name`, `students`.`surname` ASC; 
 
 
-5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti 
-
-
-6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 
-
-
-7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
-
+5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
