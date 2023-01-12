11ae71f29c5f915a103fd488e1d4256869b605c0
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2023-01-12 16:57:48 +0100

    add cool print n.4

--- join.sql
+++ join.sql
@@ -38,5 +38,25 @@ INNER JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
 ORDER BY `students`.`name`, `students`.`surname` ASC; 
 
+-- OPPURE PIU' FASHION 
 
-5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT  `students`.`id`
+       ,`students`.`name`
+       ,`students`.`surname`
+       ,`degrees`.`name`
+       ,`departments`.`name`
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `degree_id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`name`, `students`.`surname` ASC; 
+
+
+5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti 
+
+
+6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 
+
+
+7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
