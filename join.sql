59cd15287bdb3276cff64ae9a5a01776e08b531d
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2023-01-12 16:40:35 +0100

    complete join n.2

--- join.sql
+++ join.sql
@@ -8,7 +8,7 @@ WHERE `department_id` = "9";
 SELECT * FROM `degrees` INNER JOIN `students` ON `students`.`degree_id` = `degrees`.`department_id` WHERE `degrees`.`name` = "Corso di Laurea in Economia"; 
 
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze 
-
+SELECT * FROM `degrees` INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id` WHERE `degrees`.`department_id` = 7 AND `degrees`.`level` = "magistrale"; 
 
 3. Selezionare tutti i corsi IN cui insegna Fulvio Amato (id = 44) 
 SELECT * FROM `course_teacher` INNER JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id` WHERE `teacher_id` = "44"; 
